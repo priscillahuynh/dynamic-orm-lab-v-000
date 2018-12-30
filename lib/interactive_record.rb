1c78ef837b99d2234161ada53763c912d4bd5422
Priscilla H <priscillamhuynh@gmail.com>
2018-12-30 11:32:56 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -52,6 +52,6 @@ class InteractiveRecord
 
   def self.find_by(attribute.to_sym)
     sql = "SELECT * FROM #{self.table_name} WHERE '#{attribute}' = ?, attribute"
-    DB[:conn].execute(sql,self.("#{attribute}"))
+    DB[:conn].execute(sql)
   end
 end
