387e86b7c0ce14943fb26604c87281ec3c48f52e
Priscilla H <priscillamhuynh@gmail.com>
2018-12-30 11:44:25 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -51,7 +51,14 @@ class InteractiveRecord
   end
 
   def self.find_by(attribute)
-    sql = "SELECT * FROM #{self.table_name} WHERE '#{attribute}' = ?, #{attribute}"
-    DB[:conn].execute(sql)
+    column_name = attribute.keys[0].to_s
+    value_name = attribute.values[0]
+
+    sql = <<-SQL
+      SELECT * FROM #{table_name}
+      WHERE #{column_name} = ?
+      SQL
+
+    DB[:conn].execute(sql, value_name);
   end
 end
