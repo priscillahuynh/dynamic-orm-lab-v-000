c004a7c1f0c7ea1b94920bc1bb4ce8de9cf15ecd
Priscilla H <priscillamhuynh@gmail.com>
2018-12-30 11:28:46 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -52,6 +52,6 @@ class InteractiveRecord
 
   def self.find_by(attribute)
     sql = "SELECT * FROM #{self.table_name} WHERE '#{attribute}' = ?"
-    DB[:conn].execute(sql,self."#{attribute})
+    DB[:conn].execute(sql,self.#{attribute})
   end
 end
