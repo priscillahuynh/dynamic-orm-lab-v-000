8a0dd645857553185daed7072bc90a36aab70e30
Priscilla H <priscillamhuynh@gmail.com>
2018-12-30 11:22:04 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -51,7 +51,7 @@ class InteractiveRecord
   end
 
   def self.find_by(attribute)
-    sql = "SELECT * FROM #{self.table_name} WHERE #{self.col_names_for_insert} = '#{attribute}'"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{self.column_names} = '#{attribute}'"
     DB[:conn].execute(sql)
   end
 end
